a8a92c755a51a721f65ad409b2ce96794d75fe3d
Nikita <h3ikkinen@yandex.ru>
2020-10-05 03:35:54 +0300

    footer was created

--- src/js/script.js
+++ src/js/script.js
@@ -140,6 +140,92 @@ document.addEventListener('DOMContentLoaded', () => {
         scrollbarMaxSize: 0,
         direction: 'ltr',
         timeout: 1000
-    })
+    });
+    new SimpleBar(document.getElementById('info'), {
+        autoHide: true,
+        forceVisible: false,
+        classNames: {
+          resizeWrapper: 'simplebar-resize-wrapper',
+          content: 'simplebar-content',
+          offset: 'simplebar-offset',
+          mask: 'simplebar-mask',
+          wrapper: 'simplebar-wrapper',
+          placeholder: 'simplebar-placeholder',
+          scrollbar: 'simplebar-scrollbar',
+          track: 'simplebar-track',
+          heightAutoObserverWrapperEl: 'simplebar-height-auto-observer-wrapper',
+          heightAutoObserverEl: 'simplebar-height-auto-observer',
+          visible: 'simplebar-visible',
+          horizontal: 'simplebar-horizontal',
+          vertical: 'simplebar-vertical',
+          hover: 'simplebar-hover',
+          dragging: 'simplebar-dragging'
+        },
+        scrollbarMinSize: 15,
+        scrollbarMaxSize: 0,
+        direction: 'ltr',
+        timeout: 1000
+    });
+    //scroll animation: 
+    let animItems = document.querySelectorAll('._anim-items');
 
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll(params) {
+            for ( let i = 0; i < animItems.length; i++) {
+                const animItem = animItems[i];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+    
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+    
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+    
+                }
+            }
+            
+
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft};
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 500);
+    }
+    //slider 
+    var mySwiper = new Swiper('.projects__slider', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+          nextEl: '.next-btn_projects',
+          prevEl: '.prev-btn_projects',
+        },
+        spaceBetween: 60,
+        breakpoints: {
+            320: {
+              slidesPerView: 1.1,
+              spaceBetween: 20
+            },
+            1024: {
+              slidesPerView: 1.1,
+            },
+            1200: {
+                slidesPerView: 1,
+              },
+          }
+      })
 });
